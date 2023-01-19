d0a5bbf048398443a3cb7bd5f823df814a734b76
Augustine Smith <augustine.smith@amalitech.org>
2023-01-19 23:49:14 +0000

    Initialized next project and setup tailwind

---
+++ icepoker-web/src/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Icepoker - Agile Sprint Estimator</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="A tool to estimate effort or relative size of user stories in agile software development" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
